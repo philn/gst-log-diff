f2053255f5df811888a3caa39ed1300c35a20ec5
Philippe Normand <philn@igalia.com>
2019-06-16 15:13:05 +0100

    Refactor/simplify a bit

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use difference::{Changeset, Difference};
 use gst_log_parser::{parse, Entry};
 use regex::Regex;
 use std::fs::File;
-use std::io::Write;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -42,36 +41,32 @@ fn main() {
     let opt = Opt::from_args();
     let f1 = File::open(opt.input1).expect("Failed to open log file");
     let f2 = File::open(opt.input2).expect("Failed to open log file");
-
-    let mut t = term::stdout().unwrap();
-
     let category_regex = match opt.category {
         Some(pattern) => Some(Regex::new(&pattern).unwrap()),
         None => None,
     };
-    let parsed1 = parse(f1);
+
     let mut parsed2 = parse(f2);
-    for entry1 in parsed1 {
+    let mut t = term::stdout().unwrap();
+    for entry1 in parse(f1) {
         if let Some(other) = parsed2.nth(0) {
-            if category_filter(&category_regex, &entry1, &other) {
-                let Changeset { diffs, .. } = Changeset::new(&entry1.message, &other.message, "\n");
-                for item in &diffs {
-                    match item {
-                        Difference::Same(ref _x) => {
-                            t.reset().unwrap();
-                        }
-                        Difference::Add(ref x) => {
-                            t.reset().unwrap();
-                            write!(t, "{} => ", other.ts).unwrap();
-                            t.fg(term::color::GREEN).unwrap();
-                            writeln!(t, "+{}", x).unwrap();
-                        }
-                        Difference::Rem(ref x) => {
-                            t.reset().unwrap();
-                            write!(t, "{} => ", entry1.ts).unwrap();
-                            t.fg(term::color::RED).unwrap();
-                            writeln!(t, "-{}", x).unwrap();
-                        }
+            if !category_filter(&category_regex, &entry1, &other) {
+                continue;
+            }
+            let Changeset { diffs, .. } = Changeset::new(&entry1.message, &other.message, "\n");
+            for item in &diffs {
+                t.reset().unwrap();
+                match item {
+                    Difference::Same(ref _x) => {}
+                    Difference::Add(ref x) => {
+                        write!(t, "{} => ", other.ts).unwrap();
+                        t.fg(term::color::GREEN).unwrap();
+                        writeln!(t, "+{}", x).unwrap();
+                    }
+                    Difference::Rem(ref x) => {
+                        write!(t, "{} => ", entry1.ts).unwrap();
+                        t.fg(term::color::RED).unwrap();
+                        writeln!(t, "-{}", x).unwrap();
                     }
                 }
             }
